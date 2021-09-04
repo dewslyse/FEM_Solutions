df030ce612608ac196cac7d2963952c64df7ecd1
dewslyse <dewslyse@gmail.com>
2021-09-04 22:55:20 +0000

    Add error message for empty input field

--- ping-coming-soon-page/script.js
+++ ping-coming-soon-page/script.js
@@ -8,13 +8,28 @@ const emailValidation = (event) => {
 
     const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (validEmail.test(input.value)) {
-        form.submit();
+    // if (validEmail.test(input.value)) {
+    //     form.submit();
+    // }
+    // else {
+    //     form.classList.add('invalid');
+    //     errorMsg.innerHTML = "Please provide a valid email address";
+    //     input.style.border = '1px solid #ff0000';
+    // }
+    if (input.value == "") {
+        form.classList.add('invalid');
+        errorMsg.innerHTML = "Whoops! It looks like you forgot to add your email";
+        input.style.border = '1px solid #ff0000';
     }
-    else {
+
+    else if (!validEmail.test(input.value)) {
         form.classList.add('invalid');
         errorMsg.innerHTML = "Please provide a valid email address";
         input.style.border = '1px solid #ff0000';
+    }
+
+    else {
+        form.submit();
     }
 };
 

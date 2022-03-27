86027097d1199610dbdfaffc22188d3813836309
dewslyse <dewslyse@gmail.com>
2022-03-27 23:37:16 +0000

    Add initial solutions

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     })
 })
 
-//Add projects dynamically to page
+//Solutions
 const femSolutions = [
     {
         //QR code component
@@ -32,5 +32,55 @@ const femSolutions = [
         technologies: ['html', 'css'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/nft-preview-card-component/',
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/nft-preview-card-component'
+    },
+    {
+        //Article preview component
+        title: 'Article preview component',
+        featuredImg: './article-preview-component/design/desktop-preview.webp',
+        technologies: ['html', 'css', 'JS'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/article-preview-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/article-preview-component'
     }
 ];
+
+//Technologies
+function techs(techs) {
+    return `
+      <div class="technology">
+        ${techs.map((tech) => `
+            <span class="html">${tech}</span>
+            <span class="css">${tech}</span>
+            <span class="javascript">${tech}</span>`).join('')}
+      </div>
+    `;
+}
+
+//Add projects dynamically to page
+function solutions(solution) {
+    return `
+        <div class="solution newbie">
+            <div class="screenshot">
+                <img src="${solution.featuredImg}" alt="solution screenshot" loading="lazy">
+            </div>
+            <div class="solution-details">
+                ${techs(solution.technologies)}
+                <h2 class="solution-title">${solution.title}</h2>
+                <div class="links">
+                    <a class="solution-btn" href="${solution.liveURL}"
+                        target="_blank" rel="noreferrer">See solution
+                    </a>
+                    <a class="repo" href="${solution.sourceURL}"
+                        target="_blank" aria-label="github repository" rel="noreferrer">
+                        <i class="fab fa-github fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+const solutionSection = document.querySelector('.solutions-section');
+
+solutionSection.innerHTML = `
+    ${femSolutions.map(solutions).join('')}
+`;

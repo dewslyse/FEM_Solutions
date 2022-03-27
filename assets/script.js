ad7d2ffd4d18dd463d25d9936a5ca888e3197f81
dewslyse <dewslyse@gmail.com>
2022-03-27 22:14:36 +0000

    Add test projects

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-// alert("I am linked");
-
+//Filter by difficulty
 $(document).ready(function () {
     $(".level").click(function () {
         const value = $(this).attr("data-filter");
@@ -15,3 +14,23 @@ $(document).ready(function () {
         $(this).addClass("active").siblings().removeClass("active");
     })
 })
+
+//Add projects dynamically to page
+const femSolutions = [
+    {
+        //QR code component
+        title: 'QR code component',
+        featuredImg: './qr-code-component/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/qr-code-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/qr-code-component'
+    },
+    {
+        //NFT preview card component
+        title: 'NFT preview card component',
+        featuredImg: './nft-preview-card-component/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/nft-preview-card-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/nft-preview-card-component'
+    }
+];

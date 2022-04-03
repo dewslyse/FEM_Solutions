856141b2347d2137179999dc6c405a4b8600a547
dewslyse <dewslyse@gmail.com>
2022-04-03 12:44:31 +0000

    Add solutions to script.js

--- assets/script.js
+++ assets/script.js
@@ -17,8 +17,7 @@ $(document).ready(function () {
 
 //Solutions
 const femSolutions = [
-    {
-        //Testimonials grid section
+    {   //Testimonials grid section
         title: 'Testimonials grid section',
         featuredImg: './testimonials-grid-section/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -26,8 +25,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/testimonials-grid-section',
         level: 'junior'
     },
-    {
-        //QR code component
+    {   //QR code component
         title: 'QR code component',
         featuredImg: './qr-code-component/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -35,8 +33,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/qr-code-component',
         level: 'newbie'
     },
-    {
-        //NFT preview card component
+    {   //NFT preview card component
         title: 'NFT preview card component',
         featuredImg: './nft-preview-card-component/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -44,8 +41,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/nft-preview-card-component',
         level: 'newbie'
     },
-    {
-        //Order summary component
+    {   //Order summary component
         title: 'Order summary component',
         featuredImg: './order-summary-component/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -53,8 +49,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/order-summary-component',
         level: 'newbie'
     },
-    {
-        //Stats preview card
+    {   //Stats preview card
         title: 'Stats preview card',
         featuredImg: './stats-preview-card/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -62,8 +57,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/stats-preview-card',
         level: 'newbie'
     },
-    {
-        //3-column preview card
+    {   //3-column preview card
         title: '3-column preview card',
         featuredImg: './3-column-preview-card/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -71,8 +65,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/3-column-preview-card',
         level: 'newbie'
     },
-    {
-        //Profile card component
+    {   //Profile card component
         title: 'Profile card component',
         featuredImg: './profile-card-component/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -80,8 +73,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/profile-card-component',
         level: 'newbie'
     },
-    {
-        //FAQ accordion card
+    {   //FAQ accordion card
         title: 'FAQ accordion card',
         featuredImg: './faq-accordion-card/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -89,8 +81,7 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/faq-accordion-card',
         level: 'newbie'
     },
-    {
-        //Social proof section
+    {   //Social proof section
         title: 'Social proof section',
         featuredImg: './social-proof-section/design/desktop-preview.webp',
         technologies: ['html', 'css'],
@@ -98,13 +89,92 @@ const femSolutions = [
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/social-proof-section',
         level: 'newbie'
     },
-    {
-        //Article preview component
+    {   //Article preview component
         title: 'Article preview component',
         featuredImg: './article-preview-component/design/desktop-preview.webp',
         technologies: ['html', 'css', 'js'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/article-preview-component/',
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/article-preview-component',
+        level: 'newbie'
+    },
+    {   //Fylo data storage component
+        title: 'Fylo data storage component',
+        featuredImg: './fylo-data-storage-component/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/fylo-data-storage-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/fylo-data-storage-component',
+        level: 'junior'
+    },
+    {   //Four card feature section
+        title: 'Four card feature section',
+        featuredImg: './four-card-feature-section/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/four-card-feature-section/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/four-card-feature-section',
+        level: 'newbie'
+    },
+    {   //Base Apparel coming soon page
+        title: 'Base Apparel coming soon page',
+        featuredImg: './base-apparel-coming-soon/design/desktop-preview.webp',
+        technologies: ['html', 'css', 'js'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/base-apparel-coming-soon/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/base-apparel-coming-soon',
+        level: 'newbie'
+    },
+    {   //Intro component with sign-up form
+        title: 'Intro component with sign-up form',
+        featuredImg: './intro-component-with-signup/design/desktop-preview.webp',
+        technologies: ['html', 'css', 'js'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/intro-component-with-signup/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/intro-component-with-signup',
+        level: 'newbie'
+    },
+    {   //Single price grid component
+        title: 'Single price grid component',
+        featuredImg: './single-price-grid-component/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/single-price-grid-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/single-price-grid-component',
+        level: 'newbie'
+    },
+    {   //Clipboard landing page
+        title: 'Clipboard landing page',
+        featuredImg: './clipboard-landing-page/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/clipboard-landing-page/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/clipboard-landing-page',
+        level: 'junior'
+    },
+    {   //Ping coming soon page
+        title: 'Ping coming soon page',
+        featuredImg: './ping-coming-soon-page/design/desktop-preview.webp',
+        technologies: ['html', 'css', 'js'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/ping-coming-soon-page/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/ping-coming-soon-page',
+        level: 'newbie'
+    },
+    {   //Huddle landing page with alternating blocks
+        title: 'Huddle landing page with alternating blocks',
+        featuredImg: './huddle-landing-page-with-alternating-blocks/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/huddle-landing-page-with-alternating-blocks/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/huddle-landing-page-with-alternating-blocks',
+        level: 'junior'
+    },
+    {   //Fylo data theme landing page
+        title: 'Fylo data theme landing page',
+        featuredImg: './fylo-dark-theme-landing-page/design/desktop-preview.webp',
+        technologies: ['html', 'css', 'js'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/fylo-dark-theme-landing-page/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/fylo-dark-theme-landing-page',
+        level: 'junior'
+    },
+    {   //Huddle landing page
+        title: 'Huddle landing page',
+        featuredImg: './huddle-landing-page/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/huddle-landing-page/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/huddle-landing-page',
         level: 'newbie'
     }
 ];
@@ -123,7 +193,7 @@ const solutions = (solution) => {
     return `
         <article class="solution ${solution.level}">
             <div class="screenshot">
-                <img class="img" src="${solution.featuredImg}" alt="solution screenshot" loading="lazy">
+                <img class="img" src="${solution.featuredImg}" alt="solution screenshot">
             </div>
             <div class="solution-details">
                 ${techs(solution.technologies)}

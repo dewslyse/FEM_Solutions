434414541b08aedcc404985910dc857dcce11b91
dewslyse <dewslyse@gmail.com>
2022-04-03 02:33:42 +0000

    Add solutions to script.js

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,15 @@ $(document).ready(function () {
 //Solutions
 const femSolutions = [
     {
+        //Testimonials grid section
+        title: 'Testimonials grid section',
+        featuredImg: './testimonials-grid-section/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/testimonials-grid-section/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/testimonials-grid-section',
+        level: 'junior'
+    },
+    {
         //QR code component
         title: 'QR code component',
         featuredImg: './qr-code-component/design/desktop-preview.webp',
@@ -30,10 +39,64 @@ const femSolutions = [
         //NFT preview card component
         title: 'NFT preview card component',
         featuredImg: './nft-preview-card-component/design/desktop-preview.webp',
-        technologies: ['html', 'css', 'api'],
+        technologies: ['html', 'css'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/nft-preview-card-component/',
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/nft-preview-card-component',
-        level: 'junior'
+        level: 'newbie'
+    },
+    {
+        //Order summary component
+        title: 'Order summary component',
+        featuredImg: './order-summary-component/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/order-summary-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/order-summary-component',
+        level: 'newbie'
+    },
+    {
+        //Stats preview card
+        title: 'Stats preview card',
+        featuredImg: './stats-preview-card/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/stats-preview-card/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/stats-preview-card',
+        level: 'newbie'
+    },
+    {
+        //3-column preview card
+        title: '3-column preview card',
+        featuredImg: './3-column-preview-card/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/3-column-preview-card/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/3-column-preview-card',
+        level: 'newbie'
+    },
+    {
+        //Profile card component
+        title: 'Profile card component',
+        featuredImg: './profile-card-component/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/profile-card-component/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/profile-card-component',
+        level: 'newbie'
+    },
+    {
+        //FAQ accordion card
+        title: 'FAQ accordion card',
+        featuredImg: './faq-accordion-card/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/faq-accordion-card/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/faq-accordion-card',
+        level: 'newbie'
+    },
+    {
+        //Social proof section
+        title: 'Social proof section',
+        featuredImg: './social-proof-section/design/desktop-preview.webp',
+        technologies: ['html', 'css'],
+        liveURL: 'https://dewslyse.github.io/FEM_Solutions/social-proof-section/',
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/social-proof-section',
+        level: 'newbie'
     },
     {
         //Article preview component
@@ -42,7 +105,7 @@ const femSolutions = [
         technologies: ['html', 'css', 'js'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/article-preview-component/',
         sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/article-preview-component',
-        level: 'guru'
+        level: 'newbie'
     }
 ];
 

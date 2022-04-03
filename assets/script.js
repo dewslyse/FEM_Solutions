d2ccf745f4e79ad4459b1c5f3d3fb4f6bd40444d
dewslyse <dewslyse@gmail.com>
2022-04-03 01:23:04 +0000

    Fix technology colors

--- assets/script.js
+++ assets/script.js
@@ -23,44 +23,44 @@ const femSolutions = [
         featuredImg: './qr-code-component/design/desktop-preview.webp',
         technologies: ['html', 'css'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/qr-code-component/',
-        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/qr-code-component'
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/qr-code-component',
+        level: 'newbie'
     },
     {
         //NFT preview card component
         title: 'NFT preview card component',
         featuredImg: './nft-preview-card-component/design/desktop-preview.webp',
-        technologies: ['html', 'css'],
+        technologies: ['html', 'css', 'api'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/nft-preview-card-component/',
-        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/nft-preview-card-component'
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/nft-preview-card-component',
+        level: 'junior'
     },
     {
         //Article preview component
         title: 'Article preview component',
         featuredImg: './article-preview-component/design/desktop-preview.webp',
-        technologies: ['html', 'css', 'JS'],
+        technologies: ['html', 'css', 'js'],
         liveURL: 'https://dewslyse.github.io/FEM_Solutions/article-preview-component/',
-        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/article-preview-component'
+        sourceURL: 'https://github.com/dewslyse/FEM_Solutions/tree/main/article-preview-component',
+        level: 'guru'
     }
 ];
 
 //Technologies
-function techs(techs) {
+const techs = (techs) => {
     return `
       <div class="technology">
-        ${techs.map((tech) => `
-            <span class="html">${tech}</span>
-            <span class="css">${tech}</span>
-            <span class="javascript">${tech}</span>`).join('')}
+            ${techs.map((tech) => `<span class="${tech}">${tech.toUpperCase()}</span>`).join('')}
       </div>
     `;
 }
 
 //Add projects dynamically to page
-function solutions(solution) {
+const solutions = (solution) => {
     return `
-        <div class="solution newbie">
+        <article class="solution ${solution.level}">
             <div class="screenshot">
-                <img src="${solution.featuredImg}" alt="solution screenshot" loading="lazy">
+                <img class="img" src="${solution.featuredImg}" alt="solution screenshot" loading="lazy">
             </div>
             <div class="solution-details">
                 ${techs(solution.technologies)}
@@ -75,7 +75,7 @@ function solutions(solution) {
                     </a>
                 </div>
             </div>
-        </div>
+        </article>
     `;
 }
 

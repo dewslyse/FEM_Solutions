acd137a42a03cba247d250c776e80cc68f3ed163
dewslyse <dewslyse@gmail.com>
2022-04-10 20:41:45 +0000

    Remove unused variable

--- pricing-component-with-toggle/script.js
+++ pricing-component-with-toggle/script.js
@@ -1,6 +1,6 @@
 const toggleNumber = document.querySelectorAll('.annual-price');
 const toggler = document.querySelector('.checkbox');
 
-toggler.addEventListener('click', (e) => {
+toggler.addEventListener('click', () => {
     toggleNumber.forEach((item) => item.classList.toggle('hidden'));
 });
